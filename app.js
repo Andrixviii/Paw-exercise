416419a98f95beca67670c98d89409922e09977d
muhamad nopid andriansyah <muhamadnopidandriansyah@gmail.com>
2024-12-06 12:42:10 +0700

    initial commite

---
+++ app.js
@@ -0,0 +1,16 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('Service Worker registered:', registration);
+        })
+        .catch(err => {
+          console.error('Service Worker registration failed:', err);
+        });
+    });
+  }
+
+
+  document.getElementById('button').onclick = () =>{
+    window.location.href = 'about.html';
+  }
